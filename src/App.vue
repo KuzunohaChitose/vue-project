9544bbecbbba85878ea67f1dfde15918f3d4a909
KuzunohaChitose <rluo9281@gmail.com>
2025-05-06 11:30:55 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const data = reactive({
+    count: 0,
+});
+</script>
 
 <template>
-  <div>
-    首页内容
-    <!-- Header.vue -->
-    <!-- Swipper.vue -->
-    <!-- Main.vue -->
-    <!-- Footer.vue -->
-  </div>
+    <ALayout class="root">
+        <ALayoutHeader class="header">Header</ALayoutHeader>
+        <ALayoutContent class="content">
+            <ATypography>Count is: {{ data.count }}</ATypography>
+            <AButton @click="() => data.count++">Click!</AButton>
+        </ALayoutContent>
+        <ALayoutFooter class="footer">Footer</ALayoutFooter>
+    </ALayout>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.root {
+    .content {
+        text-align: center;
+    }
+}
+</style>
