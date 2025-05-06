cdf05199645ce56218a45199291e7e19e427800f
KuzunohaChitose <rluo9281@gmail.com>
2025-05-06 11:07:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    首页内容
+    <!-- Header.vue -->
+    <!-- Swipper.vue -->
+    <!-- Main.vue -->
+    <!-- Footer.vue -->
+  </div>
+</template>
+
+<style scoped></style>
